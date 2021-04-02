6ef6ad01032a8818b5100ca43e245615a10bb80e
jhuan-gg <gjhuan672@gmail.com>
2021-04-01 22:36:29 -0400

    creation database

--- servidor-node/index.js
+++ servidor-node/index.js
@@ -7,9 +7,15 @@ const uri = 'mongodb://elfoestae:root123@localhost:27017';
 MongoClient.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true}, (err, client) => {
     if (err) return console.log(err)
     console.log('o andré é um bosta')
+    const n1 = client.db("banco").collection("ColesaoBanco")
     
 })
 
 app.listen(3001, () =>
     console.log("servidor ouvindo na posta 3001")
 );
+app.post("ae",function(req, res) {
+    db.collection.insertOne({
+        
+    })
+})
