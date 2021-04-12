f5d5b01cc3d49de0fbe3c38c104c47a20382ab48
jhuan-gg <gjhuan672@gmail.com>
2021-04-11 21:38:37 -0400

    changes of insert to datas

--- jhuan/src/App.js
+++ jhuan/src/App.js
@@ -1,11 +1,21 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 import './Pure.css';
 
 function App() {
+  const [texto, setTexto] = useState('')
+  const EnviarDados = () => {
+  axios.post("http://localhost:3001/ae", {nome: texto}).then(() => {
+    console.log("deu certo")
+  })
+  }
   return (
     <div>
-      <input type="text" placeholder="o nome que tu quiser" className="pure-input-1-2"></input>
-      <button type="submit" className="button-secondary">enviar</button>
+      <input type="text" placeholder="o nome que tu quiser" value={texto} onChange={(e) => setTexto(e.target.value)} className="pure-input-1-2"></input>
+      {/* <input type="text" placeholder="qualidade" value={qualidadeTexto} className="pure-input-1-2"></input>
+      <input type="text" placeholder="digite sua idade" value={idadeTexto} className="inpure-input-1-2"></input> */}
+      <button type="submit" onClick={EnviarDados} className="button-secondary">enviar</button>
     </div>
   )
 }
