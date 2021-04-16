ee1c412a9cdfb18cc3536fb2f75b670b0a10bfc1
jhuan-gg <gjhuan672@gmail.com>
2021-04-15 21:46:50 -0400

    input: qualidade e idade funcionando

--- jhuan/src/App.js
+++ jhuan/src/App.js
@@ -5,16 +5,18 @@ import './Pure.css';
 
 function App() {
   const [texto, setTexto] = useState('')
+  const [qualidadeTexto, setQualidadeTexto] = useState('')
+  const [idadeTexto, setIdadeTexto] = useState('')
   const EnviarDados = () => {
-  axios.post("http://localhost:3001/ae", {nome: texto}).then(() => {
+  axios.post("http://localhost:3001/ae", {idade: idadeTexto, qualidade: qualidadeTexto, nome: texto}).then(() => {
     console.log("deu certo")
   })
   }
   return (
     <div>
       <input type="text" placeholder="o nome que tu quiser" value={texto} onChange={(e) => setTexto(e.target.value)} className="pure-input-1-2"></input>
-      {/* <input type="text" placeholder="qualidade" value={qualidadeTexto} className="pure-input-1-2"></input>
-      <input type="text" placeholder="digite sua idade" value={idadeTexto} className="inpure-input-1-2"></input> */}
+      <input type="text" placeholder="qualidade" value={qualidadeTexto} onChange={(e) => setQualidadeTexto(e.target.value)} className="pure-input-1-2"></input>
+      <input type="text" placeholder="digite sua idade" value={idadeTexto} onChange={(e) => setIdadeTexto(e.target.value)} className="inpure-input-1-2"></input>
       <button type="submit" onClick={EnviarDados} className="button-secondary">enviar</button>
     </div>
   )
